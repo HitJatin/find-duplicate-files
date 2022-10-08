62423f8088b6d46c867da82bd93429250e4a1ffd
TechieJatin <adriano23012001@gmail.com>
2022-10-08 17:03:50 +0530

    completed hashmap

--- find_duplicates.py
+++ find_duplicates.py
@@ -1,18 +1,14 @@
 import os
-import filecmp
-files = os.listdir("./test")
-pathOfFolder = "path of test folder"
-print(files)
-# filesPath = {}
-# for file in files:
-#     if file != file_name:
-#         if filecmp.cmp(file_name, file):
-#             os.remove(file)
-#             print('file has deleted - ', file)
-#HashMap - What it is and how it's implemented in python (Remember not to import any library)
-#How to find files in a path
-# Expected Outcome
-# test1.java - ["/"]
-# test2.java - ["/", "/practice/"]
-# test3.java - ["/", "/practice/"]
-# test4.java - ["/"]
+filesPath = {}
+def addFilesInHashMap(path):
+    files = os.listdir(path)
+    for file in files:
+        if os.path.isdir(path + "/" + file):
+            addFilesInHashMap(path + "/" + file)
+        else:
+            if filesPath.get(file) == None:
+                filesPath[file] = []
+            filesPath[file].append(path)
+            print(filesPath)
+addFilesInHashMap("./test")
+print(filesPath)
